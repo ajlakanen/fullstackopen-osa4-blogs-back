7d34951599b3f791d35682f860a4dda71dfd993c
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-07-20 20:07:20 +0300

    Tehtävä 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   return 1;
 };
 
+var blogs = [
+  { name: "a", likes: 1 },
+  { name: "b", likes: 2 },
+  { name: "c", likes: 3 },
+];
+
+const totalLikes = (blogs) => {
+  return blogs.reduce(function (acc, blog) {
+    return acc + blog.likes;
+  }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
