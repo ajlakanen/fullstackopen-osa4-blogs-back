6d349d6ebd4a761a272f14a009afb91419336013
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-07-24 09:22:18 +0300

    Käyttäjänimen oltava yksilöllinen. Salasanakriteerejä.

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 const User = require("../models/user");
 
-usersRouter.post("/", async (request, response) => {
+usersRouter.post("/", async (request, response, next) => {
   const { username, name, password } = request.body;
 
   const existingUser = await User.findOne({ username });
@@ -11,18 +11,29 @@ usersRouter.post("/", async (request, response) => {
       error: "username must be unique",
     });
   }
+
+  if (password.length <= 3) {
+    return response.status(400).json({
+      error: "password too short",
+    });
+  }
+
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  });
+  try {
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    });
 
-  const savedUser = await user.save();
+    const savedUser = await user.save();
 
-  response.status(201).json(savedUser);
+    response.status(201).json(savedUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
 usersRouter.get("/", async (request, response) => {
--- models/user.js
+++ models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  username: String,
+  username: { type: String, minlength: 3, required: true },
   name: String,
-  passwordHash: String,
+  passwordHash: { type: String, required: true },
   blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
