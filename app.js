c24ccab0ee19773d7666486dc3cdc24ac1401abf
Antti-Jussi Lakanen <antti-jussi.lakanen@jyu.fi>
2022-08-01 20:06:05 +0300

    testingRouter

--- app.js
+++ app.js
@@ -39,9 +39,14 @@ app.use(
 );
 app.use(middleware.tokenExtractor);
 
-app.use("/api/blogs", blogRouter);
-app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/blogs", blogRouter);
+
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
 
 app.use(middleware.errorHandler);
 app.use(middleware.unknownEndpoint);
